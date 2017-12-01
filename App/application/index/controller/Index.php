2e46379d9b107be070045b738a9c8e9eebd736ba
zq <1040657944@qq.com>
2017-12-01 16:34:42 +0800

    项目

---
+++ App/application/index/controller/Index.php
@@ -0,0 +1,16 @@
+<?php
+namespace app\index\controller;
+
+use think\Controller;
+use app\index\model\Username;
+/**
+*快易贷平台开发
+ */
+
+class Index extends Controller
+{
+    public function index()
+    {
+    	return $this->fetch('index');
+    }
+}
