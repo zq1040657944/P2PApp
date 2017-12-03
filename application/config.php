958fb276ee48193ebb53c980933e478560540a9d
zq <1040657944@qq.com>
2017-12-03 23:00:29 +0800

    tijiao

--- application/config.php
+++ application/config.php
@@ -141,9 +141,7 @@ return [
     ],
 
     // 视图输出字符串内容替换
-    'view_replace_str'       => [
-        '__PUBLIC__'=>DS.'P2PApp'.DS.'public'.DS.'admin'.DS,
-    ],
+    'view_replace_str'       => [],
     // 默认跳转页面对应的模板文件
     'dispatch_success_tmpl'  => THINK_PATH . 'tpl' . DS . 'dispatch_jump.tpl',
     'dispatch_error_tmpl'    => THINK_PATH . 'tpl' . DS . 'dispatch_jump.tpl',
@@ -239,22 +237,5 @@ return [
         'type'      => 'bootstrap',
         'var_page'  => 'page',
         'list_rows' => 15,
-    ],
-    //验证码配置
-    'captcha'=>[
-        // 验证码字符集合
-        'codeSet'  => '2345678abcdefhijkmnpqrstuvwxyzABCDEFGHJKLMNPQRTUVWXY',
-        // 验证码字体大小(px)
-        'fontSize' => 25,
-        // 是否画混淆曲线
-        'useCurve' => true,
-        // 验证码图片高度
-        'imageH'   => 30,
-        // 验证码图片宽度
-        'imageW'   => 100,
-        // 验证码位数
-        'length'   => 5,
-        // 验证成功后是否重置
-        'reset'    => true
     ],
 ];
