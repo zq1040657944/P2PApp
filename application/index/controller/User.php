334b61288844083d5a69b3593edd9ebe8d20a908
xqhero <990164909@qq.com>
2017-12-04 20:42:47 +0800

    test

--- application/index/controller/User.php
+++ application/index/controller/User.php
@@ -9,7 +9,7 @@ class User extends Controller{
     public $callback;
     public function login(){
         $request=Request::instance();
-        $this->callback=$request->param('callbake');
+        $this->callback=$request->param('callback');
         $username=Request::instance()->param('username');
         $pwd=Request::instance()->param('password');
         $authCode=Request::instance()->param('authCode');
@@ -19,5 +19,18 @@ class User extends Controller{
         $userModel=new \app\index\model\User();
         $Usercheck=$userModel->userLogin($username,$pwd,$authCode,$sessionCode);
         return $request->param("callback")."(".json_encode($Usercheck).")";
+    }
+    public function userReg(){
+        $request=Request::instance();
+        $this->callback=$request->param("callback");
+        $tel=$request->param("tel");
+        $password=$request->param('password');
+        //验证码
+        $authCode=$request->param("authCode");
+        //手机验证码
+        $telauthcode=$request->param("telauthcode");
+        $userModel=new \app\index\model\User();
+        $return=$userModel->modelReg($tel,$password,$authCode,$telauthcode);
+        return $request->param("callback")."(".json_encode($return).")";
     }
 }
--- application/index/model/User.php
+++ application/index/model/User.php
@@ -3,6 +3,7 @@ namespace app\index\model;
 
 use think\Db;
 use think\Model;
+use think\Session;
 
 class User extends Model{
     /**
@@ -10,37 +11,85 @@ class User extends Model{
      * @param $username,$pwd,$authCode,$sessionCode
      * return array
      * 返回规则
-     * 2001 验证码不正确
-     * 2002 用户名或密码不正确
+     * 1001 验证码不正确
+     * 1002 用户名或密码不正确
+     * 1004 电话号码已经存在
+     * 1005 手机验证码不存在
+     * 1006 入库失败
      * 200 成功返回 {userid}返回用户信息
      */
     public function userLogin($username,$pwd,$authCode,$sessionCode){
         //验证验证码是否正确
         //定义返回数组
         if($authCode!=$sessionCode){
-            return ['code'=>2001,'msg'=>'验证码错误'];
+            return ['code'=>1001,'msg'=>'验证码错误'];
         }
         //查询是手机号
         $Usertel=Db::table('p_user')->where('tel',$username)->find();
 
         if(!empty($Usertel)){
-            //用户已经注册
-//            e10adc3949ba59abbe56e057f20f883e
-//            e10adc3949ba59abbe56e057f20f883e
+
             if($Usertel['password']==md5($pwd)){
                 //用户名和密码及验证吗都正确
                 $code=200;
                 //把用户id传到前台去
                 $msg=$Usertel['id'];
             }else{
-                $code=2002;
-                $msg="密码错误";
+                $code=1002;
+                $msg="用户名密码不存在";
             }
         }else{
-            $code=2003;
-            $msg="用户名不存在";
+            $code=1002;
+            $msg="用户名密码不存在";
         }
        return ["code"=>$code,"msg"=>$msg];
+    }
+
+    /**
+     * @param $tel
+     * @return bool
+     * 手机验证码验证手机号是否存在
+     */
+    public function checkTel($tel){
+        $userTel=Db::table("p_user")->where('tel',$tel)->find();
+        if(empty($userTel)){
+            //为空返回
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public function modelReg($tel,$pwd,$authCode,$telauthcode){
+        session_start();
+        //服务器上验证码
+        $sessionCode=$_SESSION["verification"];
+        if($authCode!=$sessionCode){
+            return ['code'=>1001,'msg'=>"验证码不正确"];
+        }
+        //手机验证
+        $telnote=Session::get("note");
+        $userTel=Db::table("p_user")->where("tel",$tel)->find();
+        if(empty($userTel)){
+            //手机号码不存在
+            //判断手机验证码是否正确
+            if($telauthcode!=$telnote){
+                return ['code'=>1005,'msg'=>"手机验证码不正确"];
+            }
+            $data=['tel'=>$tel,'password'=>md5($pwd),'creattiome'=>date("Y-m-d H:i:s",time())];
+            $res=Db::table("p_user")->insert($data);
+            $lastUserid=Db::table("p_user")->getLastInsID();
+            if($res){
+               $code=200;
+               $msg=$lastUserid;
+            }else{
+                $code="1006";
+                $msg="入库失败";
+            }
+        }else{
+            $code=1004;
+            $msg="电话号码已经存在";
+        }
+        return ['code'=>$code,'msg'=>$msg];
     }
 }
 
